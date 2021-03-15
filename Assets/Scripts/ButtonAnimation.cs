1db3ab5032a66be30e35c95781d91c16b220bd35
Fusa-F <cist.b218.f.fukushi@gmail.com>
2021-03-15 21:15:43 +0900

    [init]プロジェクトの初期化

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
