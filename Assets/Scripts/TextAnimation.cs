04a237b1784c6a13b69612e4b8470dedb9b3af72
Fusa-F <cist.b218.f.fukushi@gmail.com>
2021-04-22 15:15:08 +0900

    [add]DOTextのテスト追加

---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TextAnimation : MonoBehaviour
+{
+    public Text text;
+    [SerializeField, Header("表示する文字")] private string str;
+
+    private void Awake()
+    {
+        text.text="";
+    }
+
+    private void Start()
+    {                                                                                                                                                                                       
+        text.DOText("こんにちは。\nこれはテキストアニメーションのテストです。", 3)
+            .SetEase(Ease.Linear);
+    }
+
+    private void Update()
+    {                                   
+        
+    }
+
+    private void        
+}
